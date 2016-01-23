ca0abe79b1de96b41acb8f0cba4deac1fbe6fc3c
minseokim <minseoalexkim@gmail.com>
2016-01-23 22:41:14 +0900

    [feat]: add google places autocomplete, fix content security policy on chrome extension

--- src/js/app.js
+++ src/js/app.js
@@ -2,27 +2,43 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
 
+  var input = document.getElementById('from');
 
-  var requestData = {
-    token : '3URCTQMSNEBN7MEB3Z33',
-    'location.latitude' : '37.780125',
-    'location.longitude' : '-122.404190',
-    'location.within' : '10mi'
-  };
-
-  $http({
-    method: 'GET',
-    url: 'https://www.eventbriteapi.com/v3/events/search/',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    params : requestData
-  }).then(function onSuccess(response){
-    console.log(response);
-  }, function onError(response) {
-
+  //Set up Google Maps Autocomplete API
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    var place = autocomplete.getPlace();
+    var location = {
+      lat: place.geometry.location.lat(),
+      lon: place.geometry.location.lng(),
+      address: place.formatted_address
+    };
+    // $scope.$apply();
+    getEvents(location, '10mi');
   });
 
+    //Ajax request to fetch events from Eventbrite API
+  var getEvents = function(location, range){
 
+    var requestData = {
+      token : '3URCTQMSNEBN7MEB3Z33',
+      'location.latitude' : location.lat,
+      'location.longitude' : location.lon,
+      'location.within' : range
+    };
+
+    $http({
+      method: 'GET',
+      url: 'https://www.eventbriteapi.com/v3/events/search/',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params : requestData
+    }).then(function onSuccess(response){
+      console.log(response);
+    }, function onError(response) {
+      console.log(response);
+    });
+    };
 
 }]);

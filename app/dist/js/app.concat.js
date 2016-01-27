0d0a51a3b06be54ed9417c58f44a859c8125b74f
minseokim <minseoalexkim@gmail.com>
2016-01-27 18:27:48 +0900

    [comments]: add comments

--- app/dist/js/app.concat.js
+++ app/dist/js/app.concat.js
@@ -372,49 +372,50 @@ myApp.controller('mainController', ['$scope', '$http', 'utils', function($scope,
 
   .factory('utils', function(){
 
-  //Get current date of user
+  /*getNextWeekend() gets the start and end dates for nearest weekend using moment.js
+    Returns the weekend dates in ISOString format
+    @return {weekendDates}
+  */
   var getNextWeekend = function(){
-    //start_date.range_start  Only return events with start dates after the given UTC date.
-    //start_date.range_end  Only return events with start dates before the given UTC date.
 
-    var isoString = moment().toISOString();
-
-    var dateRange = {
+    var weekendDates = {
       start : null,
       end : null
     };
 
-    //Find the nearest Friday from today
-      //set that as the start limit filter
-        //use a loop that goes from today till 7 days from now
+    /*Starting from today, find the closest Friday and set that as start date of weekend
+      add two days to it to set end date of weekend
+    */
      var days = 1;
      var today = moment().utc();
 
      while (days <= 7) {
       if (today.format('dddd') === 'Friday') {
-          dateRange.start = today;
+          weekendDates.start = today;
           break;
        }
       today.add(1, 'days').format('dddd');
       days++;
      }
-     dateRange.end = dateRange.start.clone().add(2, 'days');
+     weekendDates.end = weekendDates.start.clone().add(2, 'days');
 
-     dateRange.start = dateRange.start.toISOString().slice(0, 19) + 'Z';
-     dateRange.end = dateRange.end.toISOString().slice(0, 19) + 'Z';
+    /*Slice the strings from moment.js to match eventbrite's datatype format
+          Moment.js format :       "2016-01-31T04:47:00.671Z"
+      Eventbrite datatype format : "2010-01-31T13:00:00Z" */
+     weekendDates.start = weekendDates.start.toISOString().slice(0, 19) + 'Z';
+     weekendDates.end = weekendDates.end.toISOString().slice(0, 19) + 'Z';
 
-     return dateRange;
-    // "2016-01-31T04:47:00.671Z"
-    // "2010-01-31T13:00:00Z"
+     return weekendDates;
   };
 
+  //processEventData() receives events data from API, maps it to format we want and returns it
   var processEventData = function(events){
           /* Data we need :
           - Title
           - URL
           - Start Date
           - logo
-      */
+        */
       //events.categoryid,  events.logo.url,  events.name.text,
       //events.start.local, events.end.local, events.url
 
@@ -423,13 +424,16 @@ myApp.controller('mainController', ['$scope', '$http', 'utils', function($scope,
           categoryid: event.category_id,
           logo: event.logo,
           title : event.name.text,
-          startDate : moment(event.start.local).format("MMM Do YY"),
-          endDate : moment(event.end.local).format("MMM Do YY"),
+          startDate : moment(event.start.local).format("dddd, MMM Do"),
+          endDate : moment(event.end.local).format("dddd, MMM Do"),
           url : event.url
         };
       });
   };
 
+  /*craftRequestParams() takes location, distance and flags as input, constructs appropriate
+    request parameters and returns it
+  */
   var craftRequestParams = function(location, distance, flags){
 
     var requestData = {
